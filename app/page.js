4635e726f520f3dc474f1bf493b8af85c77ff86e
Connor deHaas <63770389+Deviouslrd@users.noreply.github.com>
2023-10-02 20:07:15 -0600

    Update page.js
    swap class for the proper className

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="min-h-screen flex-col items-center justify-between p-24">
-      <h1 class="text-3xl">CPRG 306: Web Development 2 - Assignments</h1>
+      <h1 className="text-3xl">CPRG 306: Web Development 2 - Assignments</h1>
       <StudentInfo />
       <Link href="/week2">Link to Week 2</Link><br/>
       <Link href="/week3">Link to Week 3</Link>
